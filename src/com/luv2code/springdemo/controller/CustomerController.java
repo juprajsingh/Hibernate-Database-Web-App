96eadad6947492e7f33b27bc053afe91d2a9da04
Jupraj Singh <juprajsingh1@gmail.com>
2019-08-02 23:39:57 -0400

    Added service

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,25 +5,30 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+		@Autowired
+		private CustomerService customerService;
+	
 	
 		// need to inject the customer dao
-		@Autowired
-		private CustomerDAO customerDAO;
+		//@Autowired
+		//private CustomerDAO customerDAO;
 	
-		@RequestMapping("/list")
+		@GetMapping("/list")
 		public String ListCustomer(Model theModel) {
 			
-			// get customers from the dao
-			List<Customer> theCustomers = customerDAO.getCustomers();
+			// get customers from the service
+			List<Customer> theCustomers = customerService.getCustomers();
 			
 			// add the customer to the model
 			theModel.addAttribute("customers", theCustomers);
